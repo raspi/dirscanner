59437a57151c339ee010b01d86f164d80d61a29e
Pekka Järvinen <pekka.jarvinen@gmail.com>
2018-07-20 23:35:34 +0300

    use const

--- worker.go
+++ worker.go
@@ -7,6 +7,8 @@ import (
 	"sync"
 )
 
+const DIRECTORY_BUFFER = 65536
+
 // Result of a worker
 type workerResult struct {
 	Path string
@@ -18,14 +20,15 @@ type workerInfo struct {
 	Directory string
 }
 
+// Always use New() to get proper scanner
 type DirectoryScanner struct {
-	directoryScannerJobs chan string       // Jobs (scan directory X)
-	Results              chan workerResult // Results
-	Finished             chan bool         // Scanner has finished?
-	Aborted              chan bool         // Scanner has aborted?
-	Information          chan workerInfo   // Information about scan progress
-	Errors               chan error        // Errors that happened during scanning
-	waitGroup            *sync.WaitGroup
+	directoryScannerJobs chan string            // Jobs (scan directory X)
+	Results              chan workerResult      // Results
+	Finished             chan bool              // Scanner has finished?
+	Aborted              chan bool              // Scanner has aborted?
+	Information          chan workerInfo        // Information about scan progress
+	Errors               chan error             // Errors that happened during scanning
+	waitGroup            *sync.WaitGroup        // Waits jobs to be finished
 	FileValidatorFunc    func(os.FileInfo) bool // Function for file validation
 	isInitialized        bool                   // Initializing function called?
 	isFinished           bool                   // finished?
@@ -34,7 +37,7 @@ type DirectoryScanner struct {
 // Create new directory scanner
 func New() DirectoryScanner {
 	return DirectoryScanner{
-		Results:              make(chan workerResult),
+		Results:              make(chan workerResult, 1),
 		Finished:             make(chan bool, 1),
 		Aborted:              make(chan bool, 1),
 		Information:          make(chan workerInfo),
@@ -43,6 +46,7 @@ func New() DirectoryScanner {
 		Errors:               make(chan error),
 		// Default validator:
 		FileValidatorFunc: func(os.FileInfo) bool {
+			// Accepts all files
 			return true
 		},
 		isInitialized: false,
@@ -52,7 +56,10 @@ func New() DirectoryScanner {
 
 // Initialize workers
 func (s *DirectoryScanner) Init(workerCount uint64, fileValidatorFunc func(info os.FileInfo) bool) (err error) {
-	s.directoryScannerJobs = make(chan string, 1<<16-1)
+	// Make buffer for scanner jobs
+	s.directoryScannerJobs = make(chan string, DIRECTORY_BUFFER)
+
+	// Set file validator function which filters wanted files
 	s.FileValidatorFunc = fileValidatorFunc
 
 	if workerCount == 0 {
