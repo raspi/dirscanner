907682e756abe24419ad308becd86f7641288272
slav123 <slav123@gmail.com>
2019-04-05 09:16:41 +0200

    make it work for macOS

---
+++ inode_darwin.go
@@ -0,0 +1,23 @@
+package dirscanner
+
+import (
+	"fmt"
+	"os"
+	"syscall"
+)
+
+func getInode(path string) (uint64, error) {
+	fi, err := os.Stat(path)
+
+	if err != nil {
+		return 0, fmt.Errorf(`couldn't stat'`)
+	}
+	stat, ok := fi.Sys().(*syscall.Stat_t)
+
+	if !ok {
+		return 0, fmt.Errorf(`couldn't stat'`)
+	}
+
+	return stat.Ino, nil
+
+}
